edfaa7567139f19585b2c5738c99bbac2f8c36ca
Gabriel Faria <gadfaria@gmail.com>
2024-06-27 14:41:24 -0300

    prototype

---
+++ patterns/prototype/index.js
@@ -0,0 +1,38 @@
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+
+  bark() {
+    console.log("Woof!");
+  }
+}
+
+class SuperDog extends Dog {
+  constructor(name) {
+    super(name);
+  }
+
+  fly() {
+    console.log(`Flying!`);
+  }
+}
+
+const dog1 = new SuperDog("Daisy");
+dog1.bark();
+dog1.fly();
+
+/* -------------------------------------------------- */
+console.log("--------------------------------------------------");
+
+const dog = {
+  bark() {
+    console.log(`Woof!`);
+  },
+};
+
+const pet1 = Object.create(dog);
+
+pet1.bark(); // Woof!
+console.log("Direct properties on pet1: ", Object.keys(pet1));
+console.log("Properties on pet1's prototype: ", Object.keys(pet1.__proto__));
